b256e4d695d117d60cb1e91b792b9aa2e72c17b0
DIMA_w <oddff@mail.ru>
2023-01-11 10:37:51 +0300

    Добавил удаление элемента списка по нажатию

--- components/List/List.js
+++ components/List/List.js
@@ -9,13 +9,22 @@ class List {
       `;
     ROOT_LIST.innerHTML = html;
   }
+  removeLi() {
+    let myblock = document.querySelectorAll("li");
 
-  render() {
-    let text = document.getElementById(textNote);
-    const textHtml = `
-        <li>
-            ${textNote.value}
-        </li>`;
+    myblock.forEach((block) => block.addEventListener("click", removeBlock));
+
+    function removeBlock() {
+      let block = this;
+      block.style.opacity = 1;
+      let blockId = setInterval(function () {
+        if (block.style.opacity > 0) block.style.opacity -= 0.1;
+        else {
+          clearInterval(blockId);
+          block.remove();
+        }
+      }, 60);
+    }
   }
 }
 
@@ -31,11 +40,12 @@ document.getElementById("textNote").addEventListener("keyup", function (event) {
 
 function addLi() {
   let text = document.getElementById("textNote").value;
-  if(text != ""){
+  if (text != "") {
     let ul = document.getElementById("ulList");
     let li = document.createElement("li");
     li.prepend(text);
     ul.prepend(li);
     textNote.value = "";
+    ListPage.removeLi();
   }
 }
